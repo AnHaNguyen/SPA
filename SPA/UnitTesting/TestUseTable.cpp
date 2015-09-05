76eb49d919b54f5f0f8b1c3c3b6d803d46ac08ff
AnHaNguyen <a0113038@nus.edu.sg>
2015-09-05 16:35:21 +0800

    implement unit testing

---
+++ SPA/UnitTesting/TestUseTable.cpp
@@ -0,0 +1,32 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "UseTable.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTesting
+{
+	TEST_CLASS(UseTableTest)
+	{
+	public:
+
+		TEST_METHOD(TestUseTable)
+		{
+			// TODO: Your test code here
+			UseTable useTable;
+			Assert::AreEqual(useTable.add(0, "x"), true);
+			Assert::AreEqual(useTable.add(0, "x"), false);
+			Assert::AreEqual(useTable.add(0, "y"), true);
+
+			Assert::AreEqual(useTable.getUser("x").at(0), 0);
+			string x = "x";
+			string y = "y";
+			Assert::AreEqual(useTable.getUsed(0).at(0), x);
+			Assert::AreEqual(useTable.getUsed(0).at(1), y);
+			Assert::AreEqual(useTable.size(), 1);
+			Assert::AreEqual(useTable.isContained(0), true);
+			Assert::AreEqual(useTable.isContained(1), false);
+
+		}
+	};
+}
