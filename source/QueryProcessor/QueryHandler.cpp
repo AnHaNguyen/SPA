21e5688b829cc78c482fbea1d4a9318db9e2c99d
bihuutue <bihuutue@gmail.com>
2015-09-06 17:05:10 +0800

    More on Query processor

--- source/QueryProcessor/QueryHandler.cpp
+++ source/QueryProcessor/QueryHandler.cpp
@@ -1,8 +1,9 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 #include "../QueryProcessor/QueryHandler.h"
 #include "../Preprocessor/QueryTree.h"
-
+using namespace std;
 QueryHandler::QueryHandler() {};
 QueryHandler::~QueryHandler() {};
 
@@ -16,48 +17,104 @@ bool QueryHandler::queryRec(QueryTree* query) {
 	PreResultNode* result;
 	PreSuchThatNode* suchThat;
 	PrePatternNode* pattern;
+	vector< vector<string> > symTable;
+	if (query->getSymbolTable != NULL) {
+		symTable = query->getSymbolTable;
+	}
+	else {
+		return false;
+	}
 
 	//Handle select
+	string selType = handleSelect(query, result, symTable);
+	
+	//Handle suchThat
+	vector<int> STVec = handleSuchThat(query, suchThat, symTable);
+
+	//Handle pattern
+	vector <int> PatVec = handlePattern(query, pattern, symTable);
+}
+
+string QueryHandler::handleSelect(QueryTree * query, PreResultNode * &result, vector<vector<string>> &symTable)
+{
 	if (query->getResult != NULL) {
 		result = query->getResult();
 		string rs = result->getResult;
-		if (rs == "BOOLEAN") {
-
-		}
-		if (rs == "variable") {
-
-		}
+		return getSymMean(rs, symTable);
 	}
-	//Handle suchThat
+}
+
+
+vector<int> QueryHandler::handleSuchThat(QueryTree * query, PreSuchThatNode * &suchThat, vector<vector<string>> &symTable)
+{
 	if (query->getSuchThat != NULL) {
 		suchThat = query->getSuchThat();
-		vector<int> STVec = handleST(suchThat);
-	}
+		string syn = suchThat->getSynonym();
+		string firstAtt = suchThat->getFirstAttr();
+		string secondAtt = suchThat->getSecondAttr();
+		vector<int> ansVec;
+		
+		//Handle follows
+		if (syn == "follows") {
+			ansVec = getFollows(firstAtt, secondAtt);
+		}
+		if (syn == "follows*") {
+			ansVec = getFollowsS(firstAtt, secondAtt);
+		}
 
-	//Handle pattern
+		//Handle modifies
+		if (syn == "modifies") {
+			ansVec = getModifies(firstAtt, secondAtt);
+		}
+
+		//Handle parent
+		if (syn == "parent") {
+			ansVec = getParent(firstAtt, secondAtt);
+		}
+		if (syn == "parent*") {
+			ansVec = getParentS(firstAtt, secondAtt);
+		}
+		
+		//Handle uses
+		
+
+		/*Handle affecs - next (next iteration)
+		if (syn == "affects") {
+			ansVec = getAffect(firstAtt, secondAtt);
+		}
+		if (syn == "affects*") {
+			ansVec = getAffectS(firstAtt, secondAtt);
+		}
+		if (syn == "next") {
+			ansVec = getNext(firstAtt, secondAtt);
+		}
+		if (syn == "next") {
+			ansVec = getNextS(firstAtt, secondAtt);
+		}*/
+		return ansVec;
+	}
+}
+
+vector<int> QueryHandler::handlePattern(QueryTree * query, PrePatternNode * &pattern, vector<vector<string>> &symTable)
+{
 	if (query->getPattern != NULL) {
 		pattern = query->getPattern();
 		vector<int> PTVec = handlePT(pattern);
-	}
-}
+		pair<string, string> pat = pattern->getSynonym();
+		string type = pat.first;
+		string syn = pat.second;
+		string firstAtt = pattern->getFirstAttr();
+		string secondAtt = pattern->getSecondAttr();
 
-void QueryHandler::handlePT(PrePatternNode * pattern)
-{
-	pair<string, string> pat = pattern->getSynonym();
-	string type = pat.first;
-	string syn = pat.second;
-	string firstAtt = pattern->getFirstAttr();
-	string secondAtt = pattern->getSecondAttr();
-
-	handlePType(type, firstAtt, secondAtt);
-	if (type == "BOOLEAN") {
 		handlePType(type, firstAtt, secondAtt);
+		if (type == "BOOLEAN") {
+			handlePType(type, firstAtt, secondAtt);
+		}
 	}
-
 }
 
-vector<int> QueryHandler::handlePType(std::string &type, std::string &firstAtt, std::string &secondAtt)
-{	
+vector<int> QueryHandler::handlePType(string &type, string &firstAtt, string &secondAtt)
+{
 	vector<int> ansVec;
 	if (type == "assign") {
 		ansVec = getAssign(firstAtt, secondAtt);
@@ -71,39 +128,12 @@ vector<int> QueryHandler::handlePType(std::string &type, std::string &firstAtt, 
 	return ansVec;
 }
 
-vector<int> QueryHandler::handleST(PreSuchThatNode * suchThat)
-{
-	string syn = suchThat->getSynonym();
-	string firstAtt = suchThat->getFirstAttr();
-	string secondAtt = suchThat->getSecondAttr();
-	vector<int> ansVec;
-
-	if (syn == "follows") {
-		ansVec = getFollows(firstAtt, secondAtt);
+string QueryHandler::getSymMean(string sym, vector<vector<string>> &symTable) {
+	for (vector<int>::size_type i = 0; i != symTable.size(); i++) {
+		vector<string> current = symTable[i];
+		if (find(current.begin(), current.end(), sym) != current.end()) {
+			return current[0];
+		}
+		
 	}
-	if (syn == "follows*") {
-		ansVec = getFollowsS(firstAtt, secondAtt);
-	}
-	if (syn == "modifies") {
-		ansVec = getModifies(firstAtt, secondAtt);
-	}
-	if (syn == "parent") {
-		ansVec = getParent(firstAtt, secondAtt);
-	}
-	if (syn == "parent*") {
-		ansVec = getParentS(firstAtt, secondAtt);
-	}
-	if (syn == "affects") {
-		ansVec = getAffect(firstAtt, secondAtt);
-	}
-	if (syn == "affects*") {
-		ansVec = getAffectS(firstAtt, secondAtt);
-	}
-	if (syn == "next") {
-		ansVec = getNext(firstAtt, secondAtt);
-	}
-	if (syn == "next") {
-		ansVec = getNextS(firstAtt, secondAtt);
-	}
-	return ansVec;
 }
--- source/QueryProcessor/QueryHandler.h
+++ source/QueryProcessor/QueryHandler.h
@@ -3,14 +3,18 @@
 #include"../Preprocessor/QueryTree.h"
 
 #include<string>
+#include<vector>
+using namespace std;
 
 class QueryHandler {
 public:
-		QueryHandler();
-		~QueryHandler();
-		bool queryRec(QueryTree* query);
-		vector<int> handlePT(PrePatternNode * pattern);
-		vector<int> handlePType(std::string &type, std::string &firstAtt, std::string &secondAtt);
-		vector<int> handleST(PreSuchThatNode * suchThat);
-};
+	QueryHandler();
+	~QueryHandler();
+	bool queryRec(QueryTree* query);
+	string handleSelect(QueryTree * query, PreResultNode * &result, vector<vector<string>> &symTable);
+	vector<int> handleSuchThat(QueryTree * query, PreSuchThatNode * &suchThat, vector<vector<string>> &symTable);
+	vector<int> handlePattern(QueryTree * query, PrePatternNode * &pattern, vector<vector<string>> &symTable);
+	vector<int> handlePType(string &type, string &firstAtt, string &secondAtt);
+	string QueryHandler::getSymMean(string sym, vector<vector<string>> &symTable);
+}
 #endif
