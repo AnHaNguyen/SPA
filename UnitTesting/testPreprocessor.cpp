d8a96dc68d5fbb3169a8f6732bc17d4fa10e796b
hemolin <hemolin123@hotmail.com>
2015-09-08 01:42:36 +0800

    include directory for testPreprocessor

---
+++ UnitTesting/testPreprocessor.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <iostream>
+#include <vector>
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "QueryPreprocessor.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace std;
+
+namespace TestPreprocessor
+{
+	TEST_CLASS(TestStartPreprocessor)
+	{
+	public:
+
+		TEST_METHOD(NoSuchNoPattern)
+		{
+			QueryPreprocessor pro0;
+			string declare0 = "assign a1, a2;";
+			string input0 = "select a1";
+			QueryTree* tree0 = pro0.startProcess(declare0, input0);
+
+			Assert::AreEqual(tree0->getResult()->getResult(), (string)"a1");
+			return;
+		}
+	};
+}
