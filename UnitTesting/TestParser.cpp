6a07f966936c6f1513e72074d945d789a2a98c5b
unknown <a0115431@A0115431.stu.nus.edu.sg>
2015-09-07 12:08:39 +0800

    Starting unit test and finished checking syntax for procedure name

---
+++ UnitTesting/TestParser.cpp
@@ -0,0 +1,16 @@
+#include "CppUnitTest.h"
+#include "Parser.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTesting
+{
+	TEST_CLASS(ParserTest)
+	{
+	public:
+		TEST_METHOD(TestFollowTable) {
+
+		}
+
+	};
+}
