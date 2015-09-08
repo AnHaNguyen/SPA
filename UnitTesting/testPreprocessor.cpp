8d98ebc91199722ec7a2494e236bd7574cb2621e
hemolin <hemolin123@hotmail.com>
2015-09-09 01:14:33 +0800

    more test case for preprocessor

--- UnitTesting/testPreprocessor.cpp
+++ UnitTesting/testPreprocessor.cpp
@@ -17,11 +17,12 @@ namespace TestPreprocessor
 		TEST_METHOD(NoSuchNoPattern)
 		{
 			QueryPreprocessor pro0;
-			string declare0 = "assign a1, a2;";
+			string declare0 = "assign a1,a2;";
 			string input0 = "Select a1";
 			QueryTree* tree0 = pro0.startProcess(declare0, input0);
 
-			Assert::AreEqual(tree0->getResult()->getResult(), (string)"a1");
+			Assert::AreEqual(true, tree0->getValidity());
+			Assert::AreEqual((string)"a1", tree0->getResult()->getResult());
 			return;
 		}
 
@@ -32,31 +33,81 @@ namespace TestPreprocessor
 			string input001 = "Select w such that Modifies(a1,v1) pattern a2(_,_\"x\"_)";
 			QueryTree* tree001 = pro001.startProcess(declare001, input001);
 
-			Assert::AreEqual(tree001->getResult()->getResult(), (string)"w");
-			Assert::AreEqual(tree001->getSuchThat()->getSynonym(), (string)"Modifies");
-			Assert::AreEqual(tree001->getSuchThat()->getFirstAttr(), (string)"a1");
-			Assert::AreEqual(tree001->getSuchThat()->getSecondAttr(), (string)"v1");
-			Assert::AreEqual(tree001->getPattern()->getSynonym(), (string)"a2");
-			Assert::AreEqual(tree001->getPattern()->getFirstAttr(), (string)"_");
-			Assert::AreEqual(tree001->getPattern()->getSecondAttr(), (string)"_\"x\"_");
+			Assert::AreEqual(true, tree001->getValidity());
+			Assert::AreEqual((string)"w", tree001->getResult()->getResult());
+			Assert::AreEqual((string)"Modifies", tree001->getSuchThat()->getSynonym());
+			Assert::AreEqual((string)"a1", tree001->getSuchThat()->getFirstAttr());
+			Assert::AreEqual((string)"v1", tree001->getSuchThat()->getSecondAttr());
+			Assert::AreEqual((string)"a2", tree001->getPattern()->getSynonym());
+			Assert::AreEqual((string)"_", tree001->getPattern()->getFirstAttr());
+			Assert::AreEqual((string)"_\"x\"_", tree001->getPattern()->getSecondAttr());
 			return;
 		}
 
 		TEST_METHOD(PatternSuch)
 		{
-			QueryPreprocessor pro001;
-			string declare001 = "assign a1,a2,a3;while w;stmt s1,s2;variable v1;";
-			string input001 = "Select w such that pattern a2(_,_\"x\"_)  Modifies(a1,v1)";
-			QueryTree* tree001 = pro001.startProcess(declare001, input001);
+			QueryPreprocessor pro100;
+			string declare100 = "assign a1,a2,a3;while w;stmt s1,s2;variable v1;";
+			string input100 = "Select w pattern a2(_,_\"x\"_) such that Modifies(a1,v1)";
+			QueryTree* tree100 = pro100.startProcess(declare100, input100);
 
-			Assert::AreEqual(tree001->getResult()->getResult(), (string)"w");
-			Assert::AreEqual(tree001->getSuchThat()->getSynonym(), (string)"Modifies");
-			Assert::AreEqual(tree001->getSuchThat()->getFirstAttr(), (string)"a1");
-			Assert::AreEqual(tree001->getSuchThat()->getSecondAttr(), (string)"v1");
-			Assert::AreEqual(tree001->getPattern()->getSynonym(), (string)"a2");
-			Assert::AreEqual(tree001->getPattern()->getFirstAttr(), (string)"_");
-			Assert::AreEqual(tree001->getPattern()->getSecondAttr(), (string)"_\"x\"_");
+			Assert::AreEqual(true, tree100->getValidity());
+			Assert::AreEqual((string)"w", tree100->getResult()->getResult());
+			Assert::AreEqual((string)"Modifies", tree100->getSuchThat()->getSynonym());
+			Assert::AreEqual((string)"a1", tree100->getSuchThat()->getFirstAttr());
+			Assert::AreEqual((string)"v1", tree100->getSuchThat()->getSecondAttr());
+			Assert::AreEqual((string)"a2", tree100->getPattern()->getSynonym());
+			Assert::AreEqual((string)"_", tree100->getPattern()->getFirstAttr());
+			Assert::AreEqual((string)"_\"x\"_", tree100->getPattern()->getSecondAttr());
 			return;
+		}
+
+		TEST_METHOD(noPattern)
+		{
+			QueryPreprocessor pro2;
+			string declare2 = "assign a1,a2,a3;while w;stmt s1,s2;";
+			string input2 = "Select w such that Modifies(3,\"x\") pattern a1(_,_\"x\"_)";
+			QueryTree* tree2 = pro2.startProcess(declare2, input2);
+			Assert::AreEqual(true, tree2->getValidity());
+			Assert::AreEqual((string)"w", tree2->getResult()->getResult());
+			Assert::AreEqual((string)"Modifies", tree2->getSuchThat()->getSynonym());
+			Assert::AreEqual((string)"3", tree2->getSuchThat()->getFirstAttr());
+			Assert::AreEqual((string)"\"x\"", tree2->getSuchThat()->getSecondAttr());
+			Assert::AreEqual((string)"a1", tree2->getPattern()->getSynonym());
+			Assert::AreEqual((string)"_", tree2->getPattern()->getFirstAttr());
+			Assert::AreEqual((string)"_\"x\"_", tree2->getPattern()->getSecondAttr());
+		}
+
+		TEST_METHOD(SuchThatNumberAttribute)
+		{
+			QueryPreprocessor pro3;
+			string declare3 = "assign a1,a2,a3;while w;stmt s1,s2;variable v1;";
+			string input3 = "Select w such that Modifies(a1,v1)";
+			QueryTree* tree3 = pro3.startProcess(declare3, input3);
+			Assert::AreEqual(true, tree3->getValidity());
+			Assert::AreEqual((string)"w", tree3->getResult()->getResult());
+			Assert::AreEqual((string)"Modifies", tree3->getSuchThat()->getSynonym());
+			Assert::AreEqual((string)"a1", tree3->getSuchThat()->getFirstAttr());
+			Assert::AreEqual((string)"v1", tree3->getSuchThat()->getSecondAttr());
+			Assert::AreEqual((string)"", tree3->getPattern()->getSynonym());
+			Assert::AreEqual((string)"", tree3->getPattern()->getFirstAttr());
+			Assert::AreEqual((string)"", tree3->getPattern()->getSecondAttr());
+		}
+
+		TEST_METHOD(noSuchThat)
+		{
+			QueryPreprocessor pro3;
+			string declare3 = "assign a1,a2,a3;while w;stmt s1,s2;variable v1;";
+			string input3 = "Select w such that Modifies(a1,v1)";
+			QueryTree* tree3 = pro3.startProcess(declare3, input3);
+			Assert::AreEqual(true, tree3->getValidity());
+			Assert::AreEqual((string)"w", tree3->getResult()->getResult());
+			Assert::AreEqual((string)"Modifies", tree3->getSuchThat()->getSynonym());
+			Assert::AreEqual((string)"a1", tree3->getSuchThat()->getFirstAttr());
+			Assert::AreEqual((string)"v1", tree3->getSuchThat()->getSecondAttr());
+			Assert::AreEqual((string)"", tree3->getPattern()->getSynonym());
+			Assert::AreEqual((string)"", tree3->getPattern()->getFirstAttr());
+			Assert::AreEqual((string)"", tree3->getPattern()->getSecondAttr());
 		}
 	};
 }
