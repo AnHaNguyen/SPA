711369d0172d60f794c229957cb663de4df11c52
hemolin <hemolin123@hotmail.com>
2015-09-09 02:06:54 +0800

    test on multiple spaces

--- UnitTesting/testPreprocessor.cpp
+++ UnitTesting/testPreprocessor.cpp
@@ -94,8 +94,6 @@ namespace TestPreprocessor
 			Assert::AreEqual((string)"_\"x\"_", tree3->getPattern()->getSecondAttr());
 		}
 
-
-
 		TEST_METHOD(noSuchThat)
 		{
 			QueryPreprocessor pro4;
@@ -111,5 +109,23 @@ namespace TestPreprocessor
 			Assert::AreEqual((string)"", tree4->getPattern()->getFirstAttr());
 			Assert::AreEqual((string)"", tree4->getPattern()->getSecondAttr());
 		}
+
+		TEST_METHOD(space)
+		{
+			QueryPreprocessor pro5;
+			string declare5 = "assign   a1 ,  a2 ,  a3  ;  while  w ;   stmt s1  ,  s2    ;  variable  v1  ; ";
+			string input5 = "  Select w such  that   Modifies  (  a1  , v1  )   pattern  a1(  _  ,  _ \" x \" _   )";
+			QueryTree* tree5 = pro5.startProcess(declare5, input5);
+			Assert::AreEqual(true, tree5->getValidity());
+			Assert::AreEqual((string)"w", tree5->getResult()->getResult());
+			Assert::AreEqual((string)"Modifies", tree5->getSuchThat()->getSynonym());
+			Assert::AreEqual((string)"a1", tree5->getSuchThat()->getFirstAttr());
+			Assert::AreEqual((string)"v1", tree5->getSuchThat()->getSecondAttr());
+			Assert::AreEqual((string)"a1", tree5->getPattern()->getSynonym());
+			Assert::AreEqual((string)"_", tree5->getPattern()->getFirstAttr());
+			Assert::AreEqual((string)"_\"x\"_", tree5->getPattern()->getSecondAttr());
+		}
+
+
 	};
 }
