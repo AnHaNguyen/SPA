7b5e18cfcc02a31d7077e8c418f4a00687a589ba
hemolin <hemolin123@hotmail.com>
2015-09-09 17:14:52 +0800

    AutoTester: add queryPreprocessor in testWrapper.cpp

--- AutoTester/source/TestWrapper.cpp
+++ AutoTester/source/TestWrapper.cpp
@@ -1,4 +1,5 @@
 #include "TestWrapper.h"
+#include "../../source/Preprocessor/QueryPreprocessor.h"
 
 // implementation code of WrapperFactory - do NOT modify the next 5 lines
 AbstractWrapper* WrapperFactory::wrapper = 0;
@@ -24,6 +25,9 @@ void TestWrapper::parse(std::string filename) {
 // method to evaluating a query
 void TestWrapper::evaluate(std::string query, std::list<std::string>& results){
 // call your evaluator to evaluate the query here
+	QueryPreprocessor pro;
+	pro.start(query);
+
   // ...code to evaluate query...
 
   // store the answers to the query in the results list (it is initially empty)
