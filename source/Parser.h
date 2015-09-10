a6602071f1983019a33c48205a00963f635cb592
LI QI <liqi1993@hotmail.com>
2015-09-11 01:25:19 +0800

    Added some new methods and refactoring

--- source/Parser.h
+++ source/Parser.h
@@ -9,38 +9,53 @@ using namespace std;
 //#pragma once
 class Parser
 {
+private:
+	//string errorDescription;
+	vector<string> errorTypeList = { "File cannot open ", "Missing brackets ", "Invalid Expression ", "Procedure ", "While statement ", "Call statement ", "If statement ", "Else statement ", "Assign statement" };
+	//vector<string> programLines;
+
 public:
 	Parser();
 	~Parser();
 
 	vector<string> parseInput(string fName);
 	vector<string> result(string fName);
-	bool isInteger(string const_value);
 
 	vector<string> processFile(string fName);
 	vector<string> checkSyntax(vector<string> stringList);
+
 	void processStmtType(vector<string> stringList, int i, string word);
 	void checkFirstLine(vector<string> stringList);
+
 	void checkProcedure(vector<string> stringList, int startLine);
 	void checkCall(string str);
 	void checkAssign(string str);
 	void checkIf(vector<string> stringList, int i);
 	void checkElse(vector<string> stringList, int i);
 	void checkWhile(vector<string> stringList, int i);
+
+	bool isInteger(string const_value);
 	bool isLetter(string const_value);
 	bool isAlphaNumeric(string str);
 	bool existAlphaNumeric(string str);
 	bool isName(string name);
+	bool isNameOrConstant(string expr);
 	bool isExpression(string expr);
-	vector<string> trimmedString(string str);
-	bool checkNotEmptyLine(string line);
-	string trim(string str);
+
 	vector<string> removeAllSpaces(vector<string> stringList);
 	string removeLineSpaces(string line);
+
 	int isStmtLst(vector<string> stringList, int startLine);
-	void checkAllBrackets(vector<string> stringList);
+
 	int pairedCurlyBracketsPos(vector<string> stringList, int startIndex);
 	bool isPairedRoundBrackets(string str);
+	bool checkAllBrackets(vector<string> stringList);
+
+	vector<string> trimmedList(vector <string> list);
+	bool checkNotEmptyLine(string line);
+	string trim(string str);
+
+	bool isEqualVector(vector<string> v1, vector<string> v2);
 	void error(string errorType);
 };
 
