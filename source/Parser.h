c307a2aeebe4f65f4ec47308a87a1f17f8bc4977
LI QI <liqi1993@hotmail.com>
2015-09-12 00:54:09 +0800

    Header file added and edited functions

--- source/Parser.h
+++ source/Parser.h
@@ -11,30 +11,30 @@ class Parser
 {
 private:
 	//string errorDescription;
-	vector<string> errorTypeList = { "File cannot open ", "Missing brackets ", "Invalid Expression ", "Invalid Statement List ", "Procedure syntax",
-		"Invalid naming ", "No valid procedure ",  "Call statement ", "Missing semi-colon",
-		"While statement ", "If statement ", "Else statement ", "Assign statement" };
+	vector<string> errorTypeList = { "File cannot open ", "Missing brackets ", "Invalid Expression ", "Invalid Statement List ", "Procedure syntax ",
+		"Invalid naming ", "No valid procedure ",  "Call statement ", "Missing semi-colon ", "Missing equal sign ",
+		"While statement ", "If statement ", "Else statement ", "Assign statement ", "Wrong then statement " };
 	//vector<string> programLines;
 
 public:
 	Parser();
 	~Parser();
 
-	vector<string> parseInput(string fName);
+	void parseInput(string fName);
 	vector<string> result(string fName);
 
 	vector<string> processFile(string fName);
 	vector<string> checkSyntax(vector<string> stringList);
 
-	void processStmtType(vector<string> stringList, int i, string word);
+	string processStmtType(vector<string> stringList);
 	string checkFirstLine(vector<string> stringList);
 
 	string checkProcedure(vector<string> stringList, int startLine);
 	string checkCall(vector<string> stringList, int startLine);
-	void checkAssign(string str);
-	void checkIf(vector<string> stringList, int i);
-	void checkElse(vector<string> stringList, int i);
-	void checkWhile(vector<string> stringList, int i);
+	string checkAssign(string str);
+	string checkIf(vector<string> stringList, int i);
+	string checkElse(vector<string> stringList, int i);
+	string checkWhile(vector<string> stringList, int i);
 
 	bool isInteger(string const_value);
 	bool isLetter(string const_value);
