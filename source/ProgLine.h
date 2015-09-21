70b26afd483cd57f0d33d3fa4c3df80a07f71689
Lecongthang <lecongthang.1102@gmail.com>
2015-09-22 02:03:13 +0800

    source file of ProgLine

---
+++ source/ProgLine.h
@@ -0,0 +1,30 @@
+#ifndef ProgLine_H
+#define ProgLine_H
+
+#include <string>
+#include <vector>
+
+using namespace std;
+typedef struct stmt_t{
+	string procedure;
+	string type;
+	stmt_t(string proc, string typ) {
+		procedure = proc;
+		type = typ;
+	}
+} stmt;
+
+class ProgLine {
+private: vector<stmt_t> progLine;		//list contains all stmt and its type (assign/if/while/call)
+public: 
+		ProgLine();
+		~ProgLine();
+		bool addToList(int lineNo, string type, string proc);		//add a stmt to List, return true if successful
+		string getType(int lineNo);					//get Type of a stmt
+		string getProcedure(int lineNo);			//get Procedure of a stmt
+		int numOfLines();			//return number of lines of whole program
+		vector<int> getLinesOfProc(string procedure);		//get all stmt of a proc
+		vector<int> getLinesOfType(string type);	//get all stmt of a type
+};
+
+#endif
