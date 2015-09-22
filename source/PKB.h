c868f3b98c5545011900120142afdd6a57eede43
AnHaNguyen <a0113038@nus.edu.sg>
2015-09-22 18:31:52 +0800

    Update PKB.h

--- source/PKB.h
+++ source/PKB.h
@@ -1,6 +1,4 @@
-#ifndef PKB_H
-#define PKB_H	
-
+#include "PKB.h"
 #include "AST.h"
 #include "FollowTable.h"
 #include "ModifyTable.h"
@@ -11,37 +9,116 @@
 #include "ProcTable.h"
 #include "ConstTable.h"
 
-class PKB {
-private: static FollowTable* followTable;
-		 static UseTable* useTable;
-		 static ParentTable* parentTable;
-		 static ModifyTable* modifyTable;
-		 static vector<AST*> astList;
-		 static VarTable* varTable;
-		 static ProcTable* procTable;
-		 static ConstTable* constTable;
+FollowTable* PKB::followTable;
+UseTable* PKB::useTable;
+ParentTable* PKB::parentTable;
+ModifyTable* PKB::modifyTable;
+vector<AST*> PKB::astList;
+VarTable* PKB::varTable;
+ProcTable* PKB::procTable;
+ConstTable* PKB::constTable;
+CallTable* PKB::callTable;
+NextTable* PKB::nextTable;
+vector<CFG* > PKB::cfgList;
 
-public:
-		 PKB();
-		 ~PKB();
-		 static FollowTable* getFollowTable();				//for Query
-		 static void setFollowTable(FollowTable* fTable);			//for Extractor
-		 static UseTable* getUseTable();					//for Query
-		 static void setUseTable(UseTable* useTable);				//for Extractor
-		 static ParentTable* getParentTable();				//for Query
-		 static void setParentTable(ParentTable* parTable);			//for Extractor
-		 static ModifyTable* getModifyTable();				//for Query
-		 static void setModifyTable(ModifyTable* modTable);			//for Extractor
-		 static vector<AST*> getASTList();					//for Query
-		 static void setASTList(vector<AST*> ast);					//for Extractor
-		 static VarTable* getVarTable();					//for Query
-		 static void setVarTable(VarTable* varTable);				//for Extractor
-		 static ProcTable* getProcTable();					//for Query
-		 static void setProcTable(ProcTable* procTable);			//for Extractor
-		 static ConstTable* getConstTable();				//for Query
-		 static void setConstTable(ConstTable* constTable);			//for Extractor
+PKB::PKB() {
+}
 
-		 static vector<int> checkAssign(string pattern, bool contains_);	//to check pattern
-};
+FollowTable* PKB::getFollowTable() {
+	return followTable;
+}
 
-#endif
+void PKB::setFollowTable(FollowTable* fTable) {
+	followTable = fTable;
+}
+
+UseTable* PKB::getUseTable() {
+	return useTable;
+}
+
+void PKB::setUseTable(UseTable* useTable) {
+	PKB::useTable = useTable;
+}
+
+ParentTable* PKB::getParentTable() {
+	return parentTable;
+}
+
+void PKB::setParentTable(ParentTable* parTable) {
+	PKB::parentTable = parTable;
+}
+
+ModifyTable* PKB::getModifyTable() {
+	return modifyTable;
+}
+
+void PKB::setModifyTable(ModifyTable* modTable) {
+	modifyTable = modTable;
+}
+
+vector<AST*> PKB::getASTList() {
+	return astList;
+}
+
+void PKB::setASTList(vector<AST* > astList) {
+	PKB::astList = astList;
+}
+
+VarTable* PKB::getVarTable() {
+	return varTable;
+}
+
+void PKB::setVarTable(VarTable* varTable) {
+	PKB::varTable = varTable;
+}
+
+ProcTable* PKB::getProcTable() {
+	return procTable;
+}
+
+void PKB::setProcTable(ProcTable* procTable) {
+	PKB::procTable = procTable;
+}
+
+ConstTable* PKB::getConstTable() {
+	return constTable;
+}
+
+void PKB::setConstTable(ConstTable* constTable) {
+	PKB::constTable = constTable;
+}
+
+CallTable* PKB::getCallTable() {
+	return callTable;
+}
+
+void PKB::setCallTable(CallTable* callTable) {
+	PKB::callTable = callTable;
+}
+
+NextTable* PKB::getNextTable() {
+	return nextTable;
+}
+
+void PKB::setNextTable(NextTable* nextTable) {
+	PKB::nextTable = nextTable;
+}
+
+vector<CFG*> PKB::getCFGList() {
+	return cfgList;
+}
+
+void PKB::setCFGList(vector<CFG* > cfgList) {
+	PKB::cfgList = cfgList;
+}
+
+vector<int> PKB::checkAssign(string pattern, bool contains_) {
+	vector<int> returnList;
+	//DesignExtractor ext = DesignExtractor();
+	//AST* subtree = ext.buildSubtree(pattern);
+	//for (unsigned i = 0; i < astList.size(); i++) {
+	//	vector<int> temp = astList.at(i)->findSubtree(subtree, contains_);
+	//	returnList.insert(returnList.end(), temp.begin(), temp.end());
+	//}
+	return returnList;
+}
