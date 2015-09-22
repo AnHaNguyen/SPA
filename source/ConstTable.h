f24b17ac0cf43bb3f5c0ee208488998a4caf9476
AnHaNguyen <a0113038@nus.edu.sg>
2015-09-22 18:37:46 +0800

    Update ConstTable.h

--- source/ConstTable.h
+++ source/ConstTable.h
@@ -6,11 +6,11 @@
 
 using namespace std;
 typedef struct ConstEntry_t {	
-	int line;
-	string constant;
-	ConstEntry_t(int line, string constant) {
+	string line;
+	vector<string> constants;
+	ConstEntry_t(string line, string constant) {
 		ConstEntry_t::line = line;
-		ConstEntry_t::constant = constant;
+		ConstEntry_t::constants.push_back(constant);
 	}
 }ConstEntry;
 
@@ -18,13 +18,12 @@ class ConstTable {
 private: vector<ConstEntry_t> constTable;
 public: ConstTable();
 		~ConstTable();
-		bool addToTable(int line, string constant);
-		vector<string> getConst(int line);			//return the constants used in a line (for first iter
-													//output.size() <= 1)
-		vector<int> getLine(string constant);		//return all lines use this const
-		bool isContained(string constant);		//check if a constant is in table
-		bool isContained(int line);				//check if a line is in table
-		bool isConstOfLine(int line, string constant);	//check if a line uses a constant
+		bool addToTable(string line, string constant);		//also include proc and containers
+		vector<string> getConst(string line);			//return the constants used in a line
+		vector<string> getLine(string constant);		//return all lines use this const
+		//bool isContained(string constant);		//check if a constant is in table
+		bool isContained(string line);				//check if a line is in table
+		bool isConstOfLine(string line, string constant);	//check if a line uses a constant
 		int size();
 		vector<ConstEntry_t> getTable();			//return whole table
 };
