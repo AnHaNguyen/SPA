27ab7373c0f9c5e80cea9e376ae18ab320663b22
AnHaNguyen <a0113038@nus.edu.sg>
2015-09-22 18:23:06 +0800

    Update Utility.h

--- source/Utility.h
+++ source/Utility.h
@@ -11,6 +11,10 @@ public: Utility();
 		~Utility();
 		static vector<string> removeDuplicate(vector<string> origin);
 		static vector<int> removeDuplicate(vector<int> origin);
+		static void sort(vector<int> origin);
+		static void sort(vector<string> origin);
+		static bool isContained(vector<string> list, string element);
+		static bool isContained(vector<int> list, int element);
 };
 
 #endif
