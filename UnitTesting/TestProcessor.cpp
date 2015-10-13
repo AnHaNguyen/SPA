b36d7659298dbb478e9fc443dbe0aba3d7f7aad1
DongWei <leedongwei@gmail.com>
2015-10-13 13:42:08 +0800

    Fixed QueryTree and Asserts for Parent*

--- UnitTesting/TestProcessor.cpp
+++ UnitTesting/TestProcessor.cpp
@@ -10,77 +10,27 @@ using namespace std;
 
 namespace TestProcessor {
 	TEST_CLASS(TestProcessor) {
-		public: 
-		TEST_METHOD(Processor_Trial_New) {
-			// QueryTree for "stmt s1,s2;", "Select s1 such that Parent(s1, s2);"
-			QueryTree* tree = new QueryTree();
-
-			vector<string> declarations;
-			declarations.push_back("stmt s1,s2");
-			tree->setSymbolTable(declarations);
-
-			vector<string> relations;
-			relations.push_back("Parent(s1, s2)");
-			tree->setSuchThat(relations);
-
-			vector<string> selections;
-			selections.push_back("s1");
-			tree->setResult(selections);
-
-			vector<string> empty;
-			tree->setPattern(empty);
-			
-			vector <string> code = {
-				"procedureFirst{",
-				"x=2;",
-				"z=3;}",
-				"procedureSecond{",
-				"x=0;",
-				"i=5;",
-				"whilei{",
-				"x=x+2+y;",
-				"i=i+1;}",
-				"z=z+x+i;",
-				"y=z+2;",
-				"x=x+y+z;}",
-				"procedureThird{",
-				"z=5;",
-				"v=z;}"
-			};
-
-			DesignExtractor ext = DesignExtractor(code);
-			// PKB::getParentTable();
-
-			QueryHandler handler0;
-			vector<string> results = handler0.queryRec(tree);
-
-			Assert::AreEqual(1, int(results.size()));
-			Assert::AreEqual(string("5"), results[0]);
-
-			Assert::AreEqual(1, 2);
-
-		}
-
+		public:
 		TEST_METHOD(Processor_Parent) {
 			vector<string> code = {
 				"procedureFirst{",
 				"x=1;",			// 1
 				"z=3;",			// 2
-				"whilex{",		// 3  parent
+				"while x{",		// 3  parent
 				"x=x-1;",		// 4
-				"whilex{",		// 5  p
+				"while x{",		// 5  p
 				"y=x-1;",		// 6
-				"whiley{",		// 7  p
+				"while y{",		// 7  p
 				"x=x+2;",		// 8
-				"whiley{",		// 9  p
+				"while y{",		// 9  p
 				"y=1+1;",		// 10
-				"whilex{",		// 11 p
+				"while x{",		// 11 p
 				"x=2;}}}}}}",	// 12
-				"procedureSecond{",	
+				"procedureSecond{",
 				"a=1;",			// 13
-				"whilea{",		// 14 p
+				"while a{",		// 14 p
 				"b=1+a;",		// 15
-				"whilea{",		// 16 p
+				"while a{",		// 16 p
 				"b=b-1;}}}",	// 17
 			};
 
@@ -205,20 +155,26 @@ namespace TestProcessor {
 		}
 
 		TEST_METHOD(Processor_ParentStar) {
-			vector <string> code = {
-				"TestCode{",	// 0
-				"x=1;",			// 1 
+			vector<string> code = {
+				"procedureFirst{",
+				"x=1;",			// 1
 				"z=3;",			// 2
-				"whilex{",		// 3 parent
+				"while x{",		// 3  parent
 				"x=x-1;",		// 4
-				"whilex{",		// 5 p
+				"while x{",		// 5  p
 				"y=x-1;",		// 6
-				"whiley{",		// 7 p
+				"while y{",		// 7  p
 				"x=x+2;",		// 8
-				"whiley{",		// 9 p
+				"while y{",		// 9  p
 				"y=1+1;",		// 10
-				"whilex{",		// 11 p
-				"x=2;}}}}}}"	// 12
+				"while x{",		// 11 p
+				"x=2;}}}}}}",	// 12
+				"procedureSecond{",
+				"a=1;",			// 13
+				"while a{",		// 14 p
+				"b=1+a;",		// 15
+				"while a{",		// 16 p
+				"b=b-1;}}}",	// 17
 			};
 
 			DesignExtractor ext = DesignExtractor(code);
@@ -230,7 +186,7 @@ namespace TestProcessor {
 
 
 			/**
-			 *	stmt s1,s2; 
+			 *	stmt s1,s2;
 			 *	Select s1 such that Parent*(s1, s2);
 			 */
 			QueryTree* tree1 = new QueryTree();
@@ -243,21 +199,26 @@ namespace TestProcessor {
 			relations1.push_back("Parent*(s1, s2)");
 			tree1->setSuchThat(relations1);
 
+			vector<string> selections1;
+			selections1.push_back("s1");
+			tree1->setResult(selections1);
+
 			tree1->setPattern(emptyVector);
-			tree1->setResult(emptyVector);
 
 			results = handler.queryRec(tree1);
-			Assert::AreEqual(5, int(results.size()));
+			Assert::AreEqual(7, int(results.size()));
 			Assert::AreEqual(string("3"), results[0]);
 			Assert::AreEqual(string("5"), results[1]);
 			Assert::AreEqual(string("7"), results[2]);
 			Assert::AreEqual(string("9"), results[3]);
 			Assert::AreEqual(string("11"), results[4]);
+			Assert::AreEqual(string("14"), results[5]);
+			Assert::AreEqual(string("16"), results[6]);
 
 
 
 			/**
-			 *	stmt s1; 
+			 *	stmt s1;
 			 *	Select s1 such that Parent*(s1, 8);
 			 */
 			QueryTree* tree2 = new QueryTree();
@@ -270,15 +231,18 @@ namespace TestProcessor {
 			relations2.push_back("Parent*(s1, 8)");
 			tree2->setSuchThat(relations2);
 
+			vector<string> selections2;
+			selections2.push_back("s1");
+			tree2->setResult(selections2);
+
 			tree2->setPattern(emptyVector);
-			tree2->setResult(emptyVector);
 
 			results = handler.queryRec(tree2);
 
 			Assert::AreEqual(3, int(results.size()));
-			Assert::AreEqual(string("3"), results[0]);
+			Assert::AreEqual(string("7"), results[0]);
 			Assert::AreEqual(string("5"), results[1]);
-			Assert::AreEqual(string("7"), results[2]);
+			Assert::AreEqual(string("3"), results[2]);
 
 
 
@@ -295,11 +259,14 @@ namespace TestProcessor {
 			tree3->setSymbolTable(declarations3);
 
 			vector<string> relations3;
-			relations3.push_back("Parent*(7, s1)");
+			relations3.push_back("Parent*(8, s1)");
 			tree3->setSuchThat(relations3);
 
+			vector<string> selections3;
+			selections3.push_back("s1");
+			tree3->setResult(selections3);
+
 			tree3->setPattern(emptyVector);
-			tree3->setResult(emptyVector);
 
 			results = handler.queryRec(tree3);
 
@@ -308,9 +275,9 @@ namespace TestProcessor {
 
 
 			/**
-			*	stmt s1;
-			*	Select s1 such that Parent*(7, s1);
-			*/
+			 *	stmt s1;
+			 *	Select s1 such that Parent*(7, s1);
+			 */
 			QueryTree* tree4 = new QueryTree();
 
 			vector<string> declarations4;
@@ -321,8 +288,11 @@ namespace TestProcessor {
 			relations4.push_back("Parent*(7, s1)");
 			tree4->setSuchThat(relations4);
 
+			vector<string> selections4;
+			selections4.push_back("s1");
+			tree4->setResult(selections4);
+
 			tree4->setPattern(emptyVector);
-			tree4->setResult(emptyVector);
 
 			results = handler.queryRec(tree4);
 
@@ -356,6 +326,6 @@ namespace TestProcessor {
 			Assert::AreEqual(string("6"), results[0]);
 		}
 
-		
+
 	};
 }
