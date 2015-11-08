6719f4606016b77e481a638d4c237a19713958fa
DongWei <leedongwei@gmail.com>
2015-11-08 15:45:33 +0800

    SystemTests for NextStar

--- SystemTesting/click-to-run-all-tests.bat
+++ SystemTesting/click-to-run-all-tests.bat
@@ -1,3 +1,6 @@
 ..\Debug\Autotester follows-source.txt follows-query.txt follows-output.xml
 
 ..\Debug\Autotester followsstar-source.txt followsstar-query.txt followsstar-output.xml
+
+
+..\Debug\Autotester nextstar-source.txt nextstar-query.txt nextstar-output.xm
